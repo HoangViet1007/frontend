709336692ac9cec380e4d04f74107522882214d5
Hoang Viet <hoangviet10172001@gmail.com>
2022-06-03 23:37:44 +0700

    update config

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://127.0.0.1:8000/api/";
-export const SITE_TITLE = " - Website thuê PT gym đẳng cấp nhất";
+export const API_URL = "http://api.vietbeo.xyz/api";
+export const SITE_TITLE = " - Website thuê PT Gym";
 export const UPLOAD_API_URL = "https://upload.ngon.in/upload";
 export const UPLOADCK_API_URL = "https://upload.ngon.in/ck-upload";
 export const UPLOAD_VIDEO_API_URL = "https://upload.ngon.in/upload-video";
